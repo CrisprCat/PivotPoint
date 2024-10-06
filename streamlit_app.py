86fdab61a121774e9ebeec2d8f75fe56815d74b5
CrisprCat <111688141+CrisprCat@users.noreply.github.com>
2024-10-06 16:40:45 +0000

    update navbar on every page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,28 @@
 import streamlit as st
+from modules.nav import Navbar
 
-st.set_page_config(page_title="Home", page_icon=None, layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.set_page_config(
+    page_title="PivotPoint"
+    , page_icon="🏠"
+    , layout="centered"
+    , initial_sidebar_state="auto"
+    , menu_items=None)
 
-st.title("Pivot Point 🎈")
-st.subheader("The One Where We Test")
-st.write(
-    "This app is intended to provide a comprehensive resource about statistical hypothesis testing all around AB testing.  "
-)
 
+def main():
+    Navbar()
 
-st.page_link("streamlit_app.py", label="Home", icon="🏠")
-st.page_link("pages/1_MDE ⏳.py", label="Minimum detectable effect", icon="⏳")
-st.page_link("pages/2_SRM ⚖️.py", label="Sample ratio mismatch", icon="⚖️")
-st.page_link("pages/3_Interaction detector 🕵️‍♀️.py", label="Interaction detection", icon="🕵️‍♀️")
-st.page_link("pages/4_Statistical significance 🌟.py", label="Statistical significance", icon="🌟")
-st.page_link("pages/5_FAQ ❓.py", label="Frequently asked questions", icon="❓")
+    st.title("Pivot Point 🎈")
+    st.subheader("The One Where We Test")
+    st.write(
+        "This app is intended to provide a comprehensive resource about statistical hypothesis testing all around AB testing.  "
+    )
+ 
+    st.page_link("pages/1_MDE ⏳.py", label="Minimum detectable effect", icon="⏳")
+    st.page_link("pages/2_SRM ⚖️.py", label="Sample ratio mismatch", icon="⚖️")
+    st.page_link("pages/3_Interaction detector 🕵️‍♀️.py", label="Interaction detection", icon="🕵️‍♀️")
+    st.page_link("pages/4_Statistical significance 🌟.py", label="Statistical significance", icon="🌟")
+    st.page_link("pages/5_FAQ ❓.py", label="Frequently asked questions", icon="❓")
 
+if __name__ == '__main__':
+    main()
