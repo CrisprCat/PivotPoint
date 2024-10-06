95ef2ed9dfdc303cadfc3d82a9eb93ad14e4f973
CrisprCat <111688141+CrisprCat@users.noreply.github.com>
2024-10-06 15:07:01 +0000

    Insert page structure

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Pivot Point 🎈")
+st.subheader("The One Where We Test")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "This app is intended to provide a comprehensive resource about statistical hypothesis testing all around AB testing.  "
 )
+
+
+st.page_link("streamlit_app.py", label="Home", icon="🏠")
+st.page_link("pages/1_MDE ⏳.py", label="Minimum detectable effect", icon="⏳")
+st.page_link("pages/2_SRM ⚖️.py", label="Sample ratio mismatch", icon="⚖️")
+st.page_link("pages/3_Interaction detector 🕵️‍♀️.py", label="Interaction detection", icon="🕵️‍♀️")
+st.page_link("pages/4_Statistical significance 🌟.py", label="Statistical significance", icon="🌟")
+st.page_link("pages/5_FAQ ❓.py", label="Frequently asked questions", icon="❓")
