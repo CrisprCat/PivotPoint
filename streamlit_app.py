abb916af15157021fa7e5e3c7b572b9181301944
CrisprCat <111688141+CrisprCat@users.noreply.github.com>
2024-10-06 15:47:31 +0000

    update home

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+# st.set_page_config(page_title=None, page_icon=None, layout="centered", initial_sidebar_state="auto", menu_items=None)
+
 st.title("Pivot Point 🎈")
 st.subheader("The One Where We Test")
 st.write(
@@ -7,9 +9,9 @@ st.write(
 )
 
 
-st.page_link("streamlit_app.py", label="Home", icon="🏠")
-st.page_link("pages/1_MDE ⏳.py", label="Minimum detectable effect", icon="⏳")
-st.page_link("pages/2_SRM ⚖️.py", label="Sample ratio mismatch", icon="⚖️")
-st.page_link("pages/3_Interaction detector 🕵️‍♀️.py", label="Interaction detection", icon="🕵️‍♀️")
-st.page_link("pages/4_Statistical significance 🌟.py", label="Statistical significance", icon="🌟")
-st.page_link("pages/5_FAQ ❓.py", label="Frequently asked questions", icon="❓")
+# st.page_link("streamlit_app.py", label="Home", icon="🏠")
+# st.page_link("pages/1_MDE ⏳.py", label="Minimum detectable effect", icon="⏳")
+# st.page_link("pages/2_SRM ⚖️.py", label="Sample ratio mismatch", icon="⚖️")
+# st.page_link("pages/3_Interaction detector 🕵️‍♀️.py", label="Interaction detection", icon="🕵️‍♀️")
+# st.page_link("pages/4_Statistical significance 🌟.py", label="Statistical significance", icon="🌟")
+# st.page_link("pages/5_FAQ ❓.py", label="Frequently asked questions", icon="❓")
