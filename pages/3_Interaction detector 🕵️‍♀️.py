c611c78a81a9a95212a5edfe7f91d6032d7813a2
CrisprCat <111688141+CrisprCat@users.noreply.github.com>
2024-11-02 14:34:48 +0100

    Insert Interaction detector function and update requirements accordingly

--- pages/3_Interaction detector 🕵️‍♀️.py
+++ pages/3_Interaction detector 🕵️‍♀️.py
@@ -1,9 +1,88 @@
 import streamlit as st
 from modules.functions import Navbar
+import numpy as np
+import pandas as pd
+from scipy.stats import chi2_contingency
+
+st.set_page_config(
+    page_title="PivotPoint - Interaction"
+    , page_icon="🕵️‍♀️"
+    , layout="centered"
+    , initial_sidebar_state="auto"
+    , menu_items=None)
+
 
 def main():
     Navbar()
 
+    st.title ("Interaction tester")
+
+    with st.container():
+        st.header("Traffic interactions")
+        st.subheader("Please input your data:")
+
+        colA1, colA2 = st.columns([1, 2])
+        with colA1:
+            num_of_experiments = st.number_input('How many experiments did you run in parallel?'
+                                                 , 2
+                                                 , 2
+                                                 , help = 'Currently this calculator only supports analysis of two parallel experiments.'
+                                                 )
+
+    df = pd.DataFrame(np.nan
+                      , index=['Visits in A of Experiment 2'
+                               , 'Visits in B of Experiment 2']
+                      , columns=['A'
+                                 , 'B']
+                      )
+
+    input_data = st.data_editor(data = df
+                                , num_rows = 'fixed'
+                                , column_config = {
+                                    'A' : st.column_config.NumberColumn(
+                                        label = "Visits in A of Experiment 1"
+                                        , required = True
+                                        , default = "int"
+                                    ),
+                                    'B' : st.column_config.NumberColumn(
+                                        label = "Visits in B of Experiment 1"
+                                        , required = True
+                                        , default = "int"
+                                    ),
+                                    }
+                                    )
+
+    print(input_data)
+    print(chi2_contingency(input_data))
+
+    if input_data is not None and input_data.values.sum() > 0:
+        stat, pvalue, dof, exp_freq = chi2_contingency(input_data)
+        if pvalue < 0.1:
+            st.warning(f"""The p-value is smaller than 0.1 ({round(pvalue, 3)}). A possible traffic interaction in between your experiments was detected.""")
+        else:
+            st.balloons()
+            st.success(f"""The p-value is greater than 0.1 ({round(pvalue, 3)}). No traffic interaction in between your experiments was detected. Happy analysing your test results.""")
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
+
+
+
+
+# st.title ("Metric interactions")
+
+# import pandas as pd
+
+# from statsmodels.formula.api import ols
+
+# model = ols("REVENUE ~ X_VARIATION_NAME * Y_VARIATION_NAME", data=pdf_xy_rpv)
+
+# results = model.fit()
+
+# results.summary()
