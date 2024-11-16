b993ea9e73df679369e4f18c342abf08aaf79323
CrisprCat <111688141+CrisprCat@users.noreply.github.com>
2024-11-16 17:57:56 +0100

    Add linkedin link in footer

--- modules/functions.py
+++ modules/functions.py
@@ -79,7 +79,7 @@ def footer():
         image('https://avatars3.githubusercontent.com/u/45109972?s=400&v=4',
               width=px(25), height=px(25)),
         " with ❤️ by ",
-        link("www.linkedin.com/in/dr-katharina-bursch-143a70120", "Kathi"),
+        link("https://www.linkedin.com/in/dr-katharina-bursch-143a70120", "Kathi"),
     ]
     layout(*myargs)
 
